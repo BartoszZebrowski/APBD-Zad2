6adaedddb2dd7f1f7ef500f1b7181182d5736a68
s24708 <s24708@LAB362-10>
2024-04-14 18:31:35 +0200

    Add project files.

---
+++ WebApplication1/Animals/AnimalController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Animals
+{
+    
+    public class AnimalController : Controller
+    {
+        private AnimallRepository _animallRepository;
+        public AnimalController(AnimallRepository animallRepository)
+        {
+            _animallRepository = animallRepository;
+        }
+
+        [HttpGet("/animals")]
+        public List<Animal> GetAnimals()
+        {
+            return _animallRepository.GetAllAnimals();
+        }
+    }
+}
---
+++ WebApplication1/Animals/AnimallRepository.cs
@@ -0,0 +1,45 @@
+﻿using System.Data.SqlClient;
+
+namespace WebApplication1.Animals
+{
+    public class AnimallRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        public AnimallRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public List<Animal> GetAllAnimals()
+        {
+            using var sqlConnection = new SqlConnection(_configuration["ConnectionStrings:DefaultConnetion"]);
+
+            sqlConnection.Open();
+
+            var sqlCommand = new SqlCommand("SELECT * FROM Animals");
+            sqlCommand.Connection = sqlConnection;
+            var response = sqlCommand.ExecuteReader();
+
+            var animals = new List<Animal>();
+
+            while (response.Read())
+            {
+                var animal = new Animal
+                {
+                    Id = (int)response["IdAnimal"],
+                    Name = response["Name"].ToString(),
+                    Description = response["Description"].ToString(),
+                    Category = response["Category"].ToString(),
+                    Area = response["Area"].ToString()
+                };
+
+                animals.Add(animal);
+            }
+            
+            return animals;
+        }
+
+
+    }
+}
